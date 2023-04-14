2e5ebd3d22d0f9abc08901472602047b7a4c74e1
islam <www.islam1243@gmail.com>
2023-04-14 17:00:04 +0200

    solved NextRound

--- Next Round.cpp
+++ Next Round.cpp
@@ -4,23 +4,17 @@
 using namespace std;
 
 int main (){
-//get data 
-int n , k ;
+    //get data
+int n , k, i=0 ;
 cin>> n >> k;
-int i ;
-int given[50];
-for ( i=0 ; i<n;i++){
-
-cin >>  given[i] ;
-
-}
-for (i=0;i<=n;i++){
-    
-    if (given[i]<given[k-1]||given[i]<0.5){
-        cout<< i << endl;
-        break;}
-
-}
-
-
+    int  counter=0 ; 
+    int given[n];
+    for (i = 0; i < n; i++) {
+        cin >> given[i];
+    }
+    for (i = 0; i < n; i++)
+     if (given[i] >= given[k - 1] && given[i] > 0)   counter++;
+              
+    cout << counter ;
+    return 0;
 }
