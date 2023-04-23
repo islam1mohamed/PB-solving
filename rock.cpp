20869a7c0c844aec1d899c32c927d94dc44a09ee
islam <www.islam1243@gmail.com>
2023-04-23 16:29:23 +0200

    solving colored rocks pb

---
+++ rock.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std ;
+int main(){
+    int rockNum,rockRemoved =0;
+    char rockColors;
+    cin>> rockNum ;
+    char holder='0';
+    for (int i = 0; i < rockNum; i++) {
+        cin >> rockColors;
+        if (rockColors==holder){
+            rockRemoved ++;
+        }
+        holder = rockColors;
+    }
+ cout << rockRemoved;
+
+}
