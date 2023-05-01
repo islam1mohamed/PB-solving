355d14ffb33e10f5e395287befc328646014a60f
Islam Mohamed <130303768+islam1mohamed@users.noreply.github.com>
2023-05-01 17:31:16 +0300

    daily

---
+++ Word.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int up=0;
+    int low=0;
+    string inp;
+
+    cin>> inp ;
+    for (int i=0; i<inp.size(); i++){
+    if (inp[i]<'a')      up ++;
+
+    else low--;
+    }
+    
+    if (low>=up){
+        for (int i=0; i<inp.size(); i++){
+            if (inp[i]>96){
+                inp[i]=inp[i]+'A'-'a';
+            }
+        }
+    }
+    else{
+         for (int i=0; i<inp.size(); i++){
+            if (inp[i]<96){
+                inp[i]=inp[i]+'a'-'A';
+            }
+        }
+
+    }
+    cout<< inp;
+
+}
