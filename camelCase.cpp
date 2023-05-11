e415c23c8814208666fb82bd6473597009e0cb69
Islam Mohamed <130303768+islam1mohamed@users.noreply.github.com>
2023-05-11 09:32:05 +0300

    daily

---
+++ camelCase.cpp
@@ -0,0 +1,39 @@
+//take a word from user 
+//capitalize index 0
+//return output 
+#include <iostream>
+#include<string>
+
+using namespace std;
+string to_camel_case (string &in ){
+int tot = in.length();
+    for (int i = 0 ; i< tot; i++){
+        if (in[i]== '-' ||in[i]== '_' ){
+          if(in[i+1]>'Z')in[i+1] = in[i+1] +'A'-'a';}
+    }
+
+   
+for(int i=0; i < tot; i++)
+ {if(in[i]=='-'|| in[i]=='_')
+        { for(int j=i; j<(tot-1); j++)
+                in[j] = in[j+1];
+
+            in.pop_back();
+            tot--; 
+            i--;  }
+}
+return in;
+}
+
+
+int main(int argc, string argv) {
+    // Add your test cases here
+    cout << to_camel_case("the-stealth-warrior") << endl;
+    cout << to_camel_case("The_Stealth_Warrior") << endl;
+    cout << to_camel_case("The_Stealth-Warrior") << endl;
+    
+    return 0;
+}
+
+
+
